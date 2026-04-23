73a0bc0632d788581458a7844e7f4885405e9da8
pjpauwelyn <93208893+pjpauwelyn@users.noreply.github.com>
2026-04-23 11:03:49 +0200

    cleanup: delete adaptive_pipeline.py shim (AdaptivePipeline alias lives in pipeline.py)

--- core/pipelines/adaptive_pipeline.py
+++
@@ -1,6 +0,0 @@
-"""backward-compat shim: import Pipeline from core.pipelines.pipeline."""
-from core.pipelines.pipeline import Pipeline, PipelineResult  # noqa: F401
-
-# keep old names working
-AdaptivePipeline = Pipeline
-AdaptiveResult   = PipelineResult
